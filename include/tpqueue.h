438098b06dd56d61d8b0e5f971b28256915b38f7
Sergey Mescheryakov <mescheryakovsrg@gmail.com>
2025-04-29 12:33:01 +0300

    Add files via upload

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,60 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Node {
+    T data;
+    Node* next;
+    Node(const T& data) : data(data), next(nullptr) {}
+  };
+
+  Node* head;
+  Node* tail;
+
+ public:
+  TPQueue() : head(nullptr), tail(nullptr) {}
+
+  ~TPQueue() {
+    while (head != nullptr) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+
+  void push(const T& item) {
+    Node* newNode = new Node(item);
+
+    if (!head || item.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+      if (!tail) tail = head;
+      return;
+    }
+
+    Node* current = head;
+    while (current->next && current->next->data.prior >= item.prior) {
+      current = current->next;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+    if (!newNode->next) tail = newNode;
+  }
+
+  T pop() {
+    if (!head) throw "Queue is empty";
+    Node* temp = head;
+    T item = head->data;
+    head = head->next;
+    if (!head) tail = nullptr;
+    delete temp;
+    return item;
+  }
+
+  bool isEmpty() const {
+    return head == nullptr;
+  }
 };
 
 struct SYM {
